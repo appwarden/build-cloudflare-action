2c90980094ec2ee4a10fe0f7e3b9fcfec778ff11
stevefrench39 <stevefrench39@protonmail.com>
2026-02-09 06:12:33 -0700

    update middleware name

--- src/templates/app.ts
+++ src/templates/app.ts
@@ -1,12 +1,12 @@
 export const appTemplate = `
 import {
   useContentSecurityPolicy,
-  withAppwarden,
+  createAppwardenMiddleware,
 } from "@appwarden/middleware/cloudflare"
 import { config } from './generated-config.mjs'
 
 export default {
-  fetch: withAppwarden((context) => ({
+  fetch: createAppwardenMiddleware((context) => ({
     debug: context.env.DEBUG,
     appwardenApiToken: context.env.APPWARDEN_API_TOKEN,
     appwardenApiHostname: context.env.APPWARDEN_API_HOSTNAME,
