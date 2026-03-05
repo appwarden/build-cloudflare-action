9dc0b851446b360df6d74d23a25d5aa6d7b9d95c
stevefrench39 <stevefrench39@protonmail.com>
2026-03-05 16:11:53 -0700

    chore: bumped middleware version

--- dist/index.js
+++ dist/index.js
@@ -33492,7 +33492,7 @@ ${formattedError}`
 };
 
 // src/index.ts
-var middlewareVersion = "3.9.0";
+var middlewareVersion = "3.9.1";
 var Debug = (debug3) => (msg) => {
   if (debug3) {
     console.log(msg);
