84e96af573133dd7f9a0c1a2b8d8d8ee057aa6ad
stevefrench39 <stevefrench39@protonmail.com>
2026-03-06 17:12:46 -0700

    chore: simplify middleware template

--- dist/index.js
+++ dist/index.js
@@ -32822,28 +32822,12 @@ import {
 } from "@appwarden/middleware/cloudflare"
 import { config } from './generated-config.mjs'
 
-// Compute global debug value from config - if ANY hostname has debug enabled, enable it globally
-const debugEnabled = Object.values(config).some(hostnameConfig => hostnameConfig.debug === true)
-
 export default {
   fetch: createAppwardenMiddleware((context) => ({
-    debug: debugEnabled,
+    debug: context.env.DEBUG,
     appwardenApiToken: context.env.APPWARDEN_API_TOKEN,
     appwardenApiHostname: context.env.APPWARDEN_API_HOSTNAME,
     multidomainConfig: config,
-    middleware: {
-      before: [
-        ...Object.entries(config)
-          .filter(([, hostnameConfig]) => hostnameConfig.contentSecurityPolicy)
-          .map(([hostname, hostnameConfig]) =>
-            useContentSecurityPolicy({
-              hostname,
-              mode: hostnameConfig.contentSecurityPolicy.mode,
-              directives: hostnameConfig.contentSecurityPolicy.directives,
-            })
-          ),
-      ],
-    },
   })),
 }
 `;
